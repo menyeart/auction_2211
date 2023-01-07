e4e75022872ecd1cd8b84a1aa75b945b669a3ab0
Matt Enyeart <menyeart1@gmail.com>
2023-01-07 09:23:11 -0700

    Add a first test

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -3,7 +3,10 @@ require './lib/attendee'
 require './lib/auction'
 
 describe 'auction' do
-
+  it "exists" do
+    auction = Auction.new
+    expect(auction).to be_an_instance_of(Auction)
+  end
 
 
 end
