8413ac5f0cfc4158a70ad9b410474405fbe76df2
Matt Enyeart <menyeart1@gmail.com>
2023-01-07 09:15:04 -0700

    Add a test for the name attribute

--- spec/attendee_spec.rb
+++ spec/attendee_spec.rb
@@ -7,6 +7,11 @@ describe 'attendee' do
     expect(attendee).to be_an_instance_of(Attendee)
   end
 
+  it "has a name" do
+    attendee = Attendee.new(name: 'Megan', budget: '$50')
+    expect(attendee.name).to eq("Megan")
+  end
+
 
 
 
