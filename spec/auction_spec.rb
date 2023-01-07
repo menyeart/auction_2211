d002d89434a11182797d6d947b6389c41bb8e843
Matt Enyeart <menyeart1@gmail.com>
2023-01-07 11:51:24 -0700

    Add test for close auction method, final commit

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -142,5 +142,35 @@ describe 'auction' do
     allow(auction).to receive(:date).and_return('01/01/2000')
     expect(auction.date).to eq('01/01/2000')
   end
+
+  it "can close bidding, sell items to attendees and return a list of items sold to attendees" do
+    item1 = Item.new('Chalkware Piggy Bank')
+    item2 = Item.new('Bamboo Picture Frame')
+    item3 = Item.new('Homemade Chocolate Chip Cookies')
+    item4 = Item.new('2 Days Dogsitting')
+    item5 = Item.new('Forever Stamps')
+    attendee1 = Attendee.new(name: 'Megan', budget: '$50')
+    attendee2 = Attendee.new(name: 'Bob', budget: '$75')
+    attendee3 = Attendee.new(name: 'Mike', budget: '$100')
+    auction = Auction.new
+    auction.add_item(item1)
+    auction.add_item(item2)
+    auction.add_item(item3)
+    auction.add_item(item4)
+    auction.add_item(item5)
+    item1.add_bid(attendee2, 20)
+    item1.add_bid(attendee1, 22)
+    item4.add_bid(attendee3, 50)
+    item3.add_bid(attendee2, 15)
+    expect(auction.close_auction).to eq({
+      item1 => attendee1,
+      item2 => 'Not Sold',
+      item3 => attendee2,
+      item4 => attendee3,
+      item5 => 'Not Sold'
+    })
+  end
+
 end
+  
 
