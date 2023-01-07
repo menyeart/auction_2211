d2c9f59867f66467b099e2d889096558fb14875c
Matt Enyeart <menyeart1@gmail.com>
2023-01-07 09:28:35 -0700

    Add a third test for the add item method

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -13,6 +13,16 @@ describe 'auction' do
     expect(auction.items).to eq([])
   end
 
+  it "can add items to the items list" do
+    auction = Auction.new
+    item1 = Item.new('Chalkware Piggy Bank')
+    item2 = Item.new('Bamboo Picture Frame')
+    expect(auction.items).to eq([])
+    auction.add_item(item1)
+    auction.add_item(item2)
+    expect(auction.items).to eq([item1, item2])
+  end
+
 
 end
 
