aee7f27834cb5cc516052428ee1bda1ce9888841
Matt Enyeart <menyeart1@gmail.com>
2023-01-07 09:58:04 -0700

    Correct spacing issues in several tests

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -33,10 +33,10 @@ describe 'item' do
     auction.add_item(item3)
     auction.add_item(item4)
     auction.add_item(item5)
-    expect(item1.bids). to eq({})
+    expect(item1.bids).to eq({})
     item1.add_bid(attendee2, 20)
     item1.add_bid(attendee1, 22)
-    expect(item1.bids). to eq({
+    expect(item1.bids).to eq({
       attendee2 => 20,
       attendee1 => 22
     })
@@ -59,7 +59,7 @@ describe 'item' do
     auction.add_item(item5)
     item1.add_bid(attendee2, 20)
     item1.add_bid(attendee1, 22)
-    expect(item1.current_high_bid). to eq(22)
+    expect(item1.current_high_bid).to eq(22)
   end
 
 
