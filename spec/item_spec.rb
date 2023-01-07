4897b9624fa562399a923ab7accf18bb5edffe37
Matt Enyeart <menyeart1@gmail.com>
2023-01-07 09:40:04 -0700

    Add a test for the add bid method

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -1,4 +1,6 @@
 require './lib/item'
+require './lib/attendee'
+require './lib/auction'
 
 describe 'item' do
   it "exists" do
@@ -15,6 +17,36 @@ describe 'item' do
     item = Item.new('Chalkware Piggy Bank')
     expect(item.bids).to eq({})
   end
+
+  it "can add bids to the bid list" do
+    item1 = Item.new('Chalkware Piggy Bank')
+    item2 = Item.new('Bamboo Picture Frame')
+    item3 = Item.new('Homemade Chocolate Chip Cookies')
+    item4 = Item.new('2 Days Dogsitting')
+    item5 = Item.new('Forever Stamps')
+    attendee1 = Attendee.new(name: 'Megan', budget: '$50')
+    attendee2 = Attendee.new(name: 'Bob', budget: '$75')
+    attendee3 = Attendee.new(name: 'Mike', budget: '$100')
+    auction = Auction.new
+    auction.add_item(item1)
+    auction.add_item(item2)
+    auction.add_item(item3)
+    auction.add_item(item4)
+    auction.add_item(item5)
+    expect(item1.bids). to eq({})
+    item1.add_bid(attendee2, 20)
+    item1.add_bid(attendee1, 22)
+    expect(item1.bids). to eq({
+      attendee1 => 20,
+      attendee2 => 22
+    })
+  end
+
+
+
+
+
+
 
 
 
